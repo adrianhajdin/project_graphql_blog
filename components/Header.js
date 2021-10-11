a80a919f4ee1224106998813c28a73cc29e9ceed
Sakthivel BN <sakthivelbn@softborne.com>
2021-10-11 21:33:17 +0530

    header navigation fix

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
-            <Link key={index} href={`category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
           ))}
         </div>
       </div>
