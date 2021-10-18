7432cbb3edfad8b9c8529109b421d6750725587d
Sakthivel BN <sakthivelbn@softborne.com>
2021-10-18 22:30:04 +0530

    ref to state in comment form

--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,37 +1,54 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { submitComment } from '../services';
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
+  const [formData, setFormData] = useState({ name: null, email: null, comment: null, storeData: false });
 
   useEffect(() => {
     setLocalStorage(window.localStorage);
-    nameEl.current.value = window.localStorage.getItem('name');
-    emailEl.current.value = window.localStorage.getItem('email');
+    const initalFormData = {
+      name: window.localStorage.getItem('name'),
+      email: window.localStorage.getItem('email'),
+      storeData: window.localStorage.getItem('name') || window.localStorage.getItem('email'),
+    };
+    setFormData(initalFormData);
   }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === 'checkbox') {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
 
   const handlePostSubmission = () => {
     setError(false);
-    if (!commentEl.current.value || !nameEl.current.value || !emailEl.current.value) {
+    const { name, email, comment, storeData } = formData;
+    if (!name || !email || !comment) {
       setError(true);
       return;
     }
     const commentObj = {
-      name: nameEl.current.value,
-      email: emailEl.current.value,
-      comment: commentEl.current.value,
+      name,
+      email,
+      comment,
       slug,
     };
 
-    if (storeDataEl.current.checked) {
-      localStorage.setItem('name', nameEl.current.value);
-      localStorage.setItem('email', emailEl.current.value);
+    if (storeData) {
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
     } else {
       localStorage.removeItem('name');
       localStorage.removeItem('email');
@@ -40,14 +57,15 @@ const CommentsForm = ({ slug }) => {
     submitComment(commentObj)
       .then((res) => {
         if (res.createComment) {
-          if (storeDataEl.current.checked) {
-            nameEl.current.value = localStorage.getItem('name');
-            emailEl.current.value = localStorage.getItem('email');
-          } else {
-            nameEl.current.value = '';
-            emailEl.current.value = '';
+          if (!storeData) {
+            formData.name = '';
+            formData.email = '';
           }
-          commentEl.current.value = '';
+          formData.comment = '';
+          setFormData((prevState) => ({
+            ...prevState,
+            ...formData,
+          }));
           setShowSuccessMessage(true);
           setTimeout(() => {
             setShowSuccessMessage(false);
@@ -60,15 +78,15 @@ const CommentsForm = ({ slug }) => {
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
-        <textarea ref={commentEl} className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
+        <textarea value={formData.comment} onChange={onInputChange} className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
-        <input type="text" ref={nameEl} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
-        <input type="email" ref={emailEl} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
+        <input type="text" value={formData.name} onChange={onInputChange} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
+        <input type="email" value={formData.email} onChange={onInputChange} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
       </div>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
-          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true" />
+          <input checked={formData.storeData} onChange={onInputChange} type="checkbox" id="storeData" name="storeData" value="true" />
           <label className="text-gray-500 cursor-pointer" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
         </div>
       </div>
