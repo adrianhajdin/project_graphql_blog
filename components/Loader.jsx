18459c9d0885ba9f30a516c68225cab8f9d4a88e
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-11-04 09:18:29 +0100

    final refactor

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,10 +4,7 @@ const Loader = () => (
   <div className="text-center">
     <button
       type="button"
-      className={`
-            inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white 
-            bg-rose-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150 cursor-not-allowed
-            `}
+      className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-rose-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150 cursor-not-allowed"
       disabled=""
     >
       <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
