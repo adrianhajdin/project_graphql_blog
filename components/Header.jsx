24075fcc5109feb0bd2ccec32ccf35b696ec424d
tharoonb <89992077+tharoonb@users.noreply.github.com>
2022-09-07 22:28:18 -0400

    initial

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="cursor-pointer font-bold text-4xl text-white">Word of the Day Club</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
