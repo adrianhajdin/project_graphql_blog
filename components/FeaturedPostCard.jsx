9568bb42b7b79bd33455a988ff1febf113659aa4
tharoonb <89992077+tharoonb@users.noreply.github.com>
2022-09-20 12:47:30 -0400

    dEPLOY

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -8,8 +8,8 @@ const FeaturedPostCard = ({ post }) => (
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
-      <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
-      <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
+      <p className="text-grey mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+      <p className="text-grey mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
       <div className="flex items-center absolute bottom-5 w-full justify-center">
         <Image
           unoptimized
@@ -19,7 +19,7 @@ const FeaturedPostCard = ({ post }) => (
           className="align-middle drop-shadow-lg rounded-full"
           src={post.author.photo.url}
         />
-        <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>
+        <p className="inline align-middle text-grey text-shadow ml-2 font-medium">{post.author.name}</p>
       </div>
     </div>
     <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
